8a562a756dfcb2f49062c69cfd10dc130c7281b5
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-02-18 11:54:39 +0600

    rectangle calculation

--- js/calculate.js
+++ js/calculate.js
@@ -29,28 +29,28 @@ if (typeof a === 'NaN' && typeof b === 'NaN' ){
 })
 // rectangle 
 
-document.getElementById('cal-btn').addEventListener('click', function(){
-const  baseFieldTriangle = document.getElementById('b-field');
-const baseFieldTrianglestring = baseFieldTriangle.value;
-const newbaseFieldTriangle = parseFloat(baseFieldTrianglestring)
+document.getElementById('cal-btn-r').addEventListener('click', function(){
+const  widthFieldrectangle = document.getElementById('w-field');
+const widthFieldrectanglestring = widthFieldrectangle.value;
+const newwidthFieldrectangle = parseFloat(widthFieldrectanglestring)
 
-const  heightFieldTriangle = document.getElementById('h-field');
-const heightFieldTrianglestring = heightFieldTriangle.value;
-const newheightFieldTriangle = parseFloat(heightFieldTrianglestring)
+const  lengthFieldrectangle = document.getElementById('l-field');
+const lengthFieldrectanglestring = lengthFieldrectangle.value;
+const newlengthFieldrectangle = parseFloat(lengthFieldrectanglestring)
 
-a= typeof newbaseFieldTriangle;
-b= typeof newheightFieldTriangle;
+a= typeof newwidthFieldrectangle;
+b= typeof newlengthFieldrectangle;
 
 if (typeof a === 'NaN' && typeof b === 'NaN' ){
     
     alert ('enter number')
    }
    else{
-    const totalTriangleField = 0.5*newbaseFieldTriangle*newheightFieldTriangle;
+    const totalrectangleField = newwidthFieldrectangle*newlengthFieldrectangle;
    
 
-    const displayfield = document.getElementById('t-field');
-    displayfield.innerText = totalTriangleField;
+    const displayfieldr = document.getElementById('t-field-r');
+    displayfieldr.innerText = totalrectangleField;
 
    }
 
